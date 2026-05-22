6d68fced9746107e71fafc719e2504012a9e6ae7
Deveshkirad <deveshkirad4@gmail.com>
2026-05-22 23:08:15 +0530

    Assignment5

--- first.py
+++ first.py
@@ -1,3 +1,10 @@
-print("Hello, World!")
-print("Welcome to Python programming.")
+import sqlite3
 
+conn = sqlite3.connect('example.db')
+sql='''insert into stocks values ('2006-01-05','BUY','RHAT',100,35.14)'''
+c = conn.cursor()
+
+c.execute(sql)
+
+conn.commit()
+conn.close()
