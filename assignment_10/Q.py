2be5f86ef64bf2d57ea047fc68fbb30899bc332f
Deveshkirad <deveshkirad4@gmail.com>
2026-05-30 21:24:53 +0530

    assignment 10

--- assignment_10/Q.py
+++ assignment_10/Q.py
@@ -24,3 +24,20 @@ print("After moving axes of 3D array:")
 print(new_arr_3d)
 
 #Q3
+arr= np.array([[1, 2, np.nan], [4, np.nan, 6], [7, 8, 9]])
+col_mean = np.nanmean(arr, axis=0)
+print("Column means:", col_mean)
+inds = np.where(np.isnan(arr))
+print("Indices of NaN values:", inds)
+arr[inds] = np.take(col_mean, inds[1])
+print("After replacing NaN values with average of columns:")
+print(arr)
+
+
+#Q4
+arr = np.array([[1, -2, 3], [-4, 5, -6], [7, -8, 9]])
+print("Original array:")
+print(arr)
+np.place(arr, arr < 0, 0)
+print("After replacing negative values with zero:")
+print(arr)
